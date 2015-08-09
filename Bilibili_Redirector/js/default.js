a3888b68aee9c3b9a95221ae7eb9400f6d9273ca
John Tan <johnmave126@gmail.com>
2015-08-10 03:27:23 +0800

    First Commit

---
+++ Bilibili_Redirector/js/default.js
@@ -0,0 +1,67 @@
+﻿(function () {
+    "use strict";
+    //Remote notification schema URL
+    //This is my own server, and it may subject to change. Set up your own if possible.
+    var REMOTE_URL = 'http://d3elem.youmu.moe/bilibili/notification.php?id=';
+
+	var app = WinJS.Application;
+	var activation = Windows.ApplicationModel.Activation;
+	var activationKinds = Windows.ApplicationModel.Activation.ActivationKind;
+	var notifications = Windows.UI.Notifications;
+
+	app.onactivated = function (args) {
+	    var activationKind = args.detail.kind;
+	    var activatedEventArgs = args.detail.detail;
+
+	    switch (activationKind) {
+	        case activationKinds.launch:
+	            var p = WinJS.UI.processAll().
+                    then(function () {
+                        //Do things and quit
+                        startPolling();
+                        launchURI();
+                    });
+
+	            // Calling done on a promise chain allows unhandled exceptions to propagate.
+	            p.done();
+
+	            // Use setPromise to indicate to the system that the splash screen must not be torn down
+	            // until after processAll and navigate complete asynchronously.
+	            args.setPromise(p);
+	            break;
+
+	        default:
+	            break;
+	    }
+
+	}
+
+	function startPolling() {
+	    var urisToPoll = [];
+
+        //Construct polling uris
+	    for (var i = 1; i <= 5; i++) {
+	        urisToPoll.push(new Windows.Foundation.Uri(REMOTE_URL + i));
+	    }
+
+	    var recurrence = notifications.PeriodicUpdateRecurrence.halfHour;
+	    var notifier = notifications.TileUpdateManager.createTileUpdaterForApplication();
+        //Enable notification queue to store up to 5 tiles
+	    notifier.enableNotificationQueue(true);
+	    //First stop previous polling
+	    notifier.stopPeriodicUpdate();
+        //Re-enable it
+	    notifier.startPeriodicUpdateBatch(urisToPoll, recurrence);
+	}
+
+	function launchURI() {
+	    var bilibili_url = 'http://www.bilibili.com/video/bangumi.html';
+	    var uri = new Windows.Foundation.Uri(bilibili_url);
+        //Let Windows open the URL, then terminate
+	    Windows.System.Launcher.launchUriAsync(uri).then(function (success) {
+	        close();
+        });
+	}
+
+	app.start();
+})();
---
+++ README.md
@@ -0,0 +1,11 @@
+Bilibili redirector
+===
+OK, in fact this is not a redirector. This is my experimental UWA (Universal Windows Application). The only function this App provides is a live tile. This App supports medium, wide, and large tiles. It will crawl the update of Bilibili Bangumi page and update the live tile accordingly(well, not exactly, the crawling work is done by scripts on a remote server, see Project `placeholder`).
+
+HOWTO
+---------
+After installing the App (to install in VS, just debug it or deploy it). Find App 'Bilibli 番剧' and pin it onto start menu. You need to run it once to activate live tile.
+
+Roadmap
+---------
+If I have really really nothing to do, I may use subtiles to allow pinning different section of Bilibili on start  menu.
